a0774cae87ee63663a491e8e079380785449fcac
Edsonortega <elrockerojeff@gmail.com>
2025-03-24 14:00:28 -0600

    Dependencies deleted from main pom, added, h2 to the task serviced and swagger

--- task-service/src/main/java/com/example/Main.java
+++ task-service/src/main/java/com/example/Main.java
@@ -4,11 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @ComponentScan(basePackages = "com.example")  //Scan all the packages
-@EnableMongoRepositories("com.example.repository") //Enable the repository
+@EnableJpaRepositories("com.example.repository") //Enable the repository
 public class Main {
 
     public static void main(String[] args) {
